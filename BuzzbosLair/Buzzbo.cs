08c0341f5bfb795203e43cdeb266fa3bb92e7ce8
LAPTOP-M7T10733\jub12 <jub1234jub@gmail.com>
2023-05-03 23:11:25 +0300

    Implemented basic awakening

--- BuzzbosLair/Buzzbo.cs
+++ BuzzbosLair/Buzzbo.cs
@@ -1,5 +1,6 @@
 ﻿using FriendCore;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,7 @@ namespace BuzzbosLair
     internal class Buzzbo : MonoBehaviour
     {
 
-        private bool awakened = false;
+        private bool awakened;
 
         private AlterHealthManager _alter_hm;
         private AlterInfectedEnemyEffects _alter_blood;
@@ -19,6 +20,49 @@ namespace BuzzbosLair
         private PlayMakerFSM _control;
         private PlayMakerFSM _stun_control;
 
+        void Awake()
+        {
+            _alter_hm = gameObject.AddComponent<AlterHealthManager>();
+            _alter_blood = gameObject.AddComponent<AlterInfectedEnemyEffects>();
 
+        }
+
+        void Start()
+        {
+
+            SetAwakened(false);
+
+            StartCoroutine(TestTimer());
+
+        }
+
+        public void SetAwakened(bool toAwakened)
+        {
+
+            if (awakened == toAwakened) return;
+
+            if (toAwakened)
+            {
+                gameObject.GetComponent<tk2dSprite>().GetCurrentSpriteDef().material.mainTexture = BuzzbosLair.GetSprite(TextureStrings.BuzzboAwakened_Key).texture;
+                _alter_blood.SetColor(Presets.Colors.lifeblood);
+            }
+            else
+            {
+                gameObject.GetComponent<tk2dSprite>().GetCurrentSpriteDef().material.mainTexture = BuzzbosLair.GetSprite(TextureStrings.BuzzboNormal_Key).texture;
+                _alter_blood.SetColor(Presets.Colors.hiveblood);
+            }
+
+            awakened = toAwakened;
+
+        }
+
+        IEnumerator TestTimer()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(3);
+                SetAwakened(!awakened);
+            }
+        }
     }
 }
